a6c6dd31a6b4d3b15211482539ac842dbfc86d8d
aniket sharma <aniket965.as@gmail.com>
2017-03-26 05:05:59 +0530

    added plan

---
+++ app/src/main/java/com/scibot/scibots/musiclist/MainActivity.java
@@ -0,0 +1,41 @@
+package com.scibot.scibots.musiclist;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+//                songList.add(new Song(thisId, thisTitle, thisArtist));
+                Log.d("who is ","id"+ thisId+" this Title " + thisTitle);
+            }
+            while (musicCursor.moveToNext());
+        }
+
+
+
+    }
+}
